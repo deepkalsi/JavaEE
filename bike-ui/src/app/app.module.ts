9ba97a7b73f70b26ab79cdfb95392e4e014aeeef
Deepak Kalsi <deepakkalsi13@gmail.com>
2021-02-23 22:24:45 -0500

    back-end plus UI

---
+++ bike-ui/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {BikeService} from './services/bike.service';
+import { AdminComponent } from './components/admin/admin.component';
+import { HomeComponent } from './components/home/home.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule 
+  ],
+  providers: [BikeService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
